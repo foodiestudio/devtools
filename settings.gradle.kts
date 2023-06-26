3c489f75d623c80e14d0a4c431be15ee7546a9af
Jiangc <jiangc@vistring.com>
2023-06-26 11:40:30 +0800

    feat: use shared versions catalog

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+    versionCatalogs {
+        create("sharedLibs") {
+            from("io.github.foodiestudio:libs-versions:2023.01.00")
+        }
+    }
+}
+rootProject.name = "DevTools"
+include(":dev-tools")
