4e6177791b3182a0f1f1106660420abccec0b704
Jiangc <jiangc@vistring.com>
2023-08-20 22:52:15 +0800

    feat: app files screen

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,6 +18,8 @@ dependencyResolutionManagement {
             from("io.github.foodiestudio:libs-versions:2023.01.00")
             library("startup", "androidx.startup:startup-runtime:1.1.1")
             library("accompanist-systemuicontroller", "com.google.accompanist:accompanist-systemuicontroller:0.30.1")
+            library("okio", "com.squareup.okio:okio:3.5.0")
+            library("navigation", "androidx.navigation:navigation-compose:2.6.0")
         }
     }
 }
