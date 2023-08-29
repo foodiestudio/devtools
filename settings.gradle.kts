3cb5330204d67c59058bdb8d663ab5371656b95f
KoShin <koshin.1116@gmail.com>
2023-08-29 22:51:28 +0800

    chore: upgrade to libs-versions:2023.04.00

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,13 +15,12 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("sharedLibs") {
-            from("io.github.foodiestudio:libs-versions:2023.01.00")
+            from("io.github.foodiestudio:libs-versions:2023.04.00")
             library("startup", "androidx.startup:startup-runtime:1.1.1")
             val accompanistVersion = "0.30.1"
             library("accompanist-systemuicontroller", "com.google.accompanist:accompanist-systemuicontroller:$accompanistVersion")
             library("accompanist-navigation-material", "com.google.accompanist:accompanist-navigation-material:$accompanistVersion")
-            library("okio", "com.squareup.okio:okio:3.5.0")
-            library("navigation", "androidx.navigation:navigation-compose:2.6.0")
+            library("okio", "com.squareup.okio:okio:3.3.0")
         }
     }
 }
