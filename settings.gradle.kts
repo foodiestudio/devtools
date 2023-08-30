e83ef3795267efec07991b9eb4294e2ac4d24461
Jiangc <jiangc@vistring.com>
2023-08-30 10:57:27 +0800

    chore: upgrade libs-versions to 2023.04.01

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,12 +15,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("sharedLibs") {
-            from("io.github.foodiestudio:libs-versions:2023.04.00")
-            library("startup", "androidx.startup:startup-runtime:1.1.1")
-            val accompanistVersion = "0.30.1"
-            library("accompanist-systemuicontroller", "com.google.accompanist:accompanist-systemuicontroller:$accompanistVersion")
-            library("accompanist-navigation-material", "com.google.accompanist:accompanist-navigation-material:$accompanistVersion")
-            library("okio", "com.squareup.okio:okio:3.3.0")
+            from("io.github.foodiestudio:libs-versions:2023.04.01")
         }
     }
 }
