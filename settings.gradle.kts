42b73e0290dd5c3102df266bea55c36b47516750
KoShin <koshin.1116@gmail.com>
2023-09-18 23:12:27 +0800

    chore: add TODO

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,6 +8,7 @@ pluginManagement {
     resolutionStrategy {
         eachPlugin {
             if (requested.id.id.startsWith("foodiestudio")) {
+                // TODO: convention-plugins:0.3.0 依赖 libs-versions:2023.04.00，需使用 libs-versions:2023.04.01 的版本
                 useModule("com.github.foodiestudio:convention-plugins:0.3.0")
             }
         }
