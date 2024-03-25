287d4c3776329b32017d4cf43e68d5708ef382fc
Ko Shin <koshin.1116@gmail.com>
2024-03-25 19:01:53 +0800

    refactor: update file provider

--- devtools/src/main/java/com/github/foodiestudio/devtools/internal/DevToolsFileProvider.kt
+++
@@ -1,6 +0,0 @@
-package com.github.foodiestudio.devtools.internal
-
-import androidx.core.content.FileProvider
-import com.github.foodiestudio.devtools.R
-
-class DevToolsFileProvider : FileProvider(R.xml.file_paths)
