36362272bf74a363499070e714e70145f4d21e2e
Ko Shin <koshin.1116@gmail.com>
2024-11-28 15:34:20 +0800

    chore: update library configuration
    - Move common library configuration to individual modules.
    - Update publishing configuration for consistency.
    - Remove unused namespace configuration from scaffold
     plugin.

--- buildSrc/src/main/kotlin/scaffold.gradle.kts
+++ buildSrc/src/main/kotlin/scaffold.gradle.kts
@@ -1,4 +1,3 @@
-import com.android.build.api.dsl.ApplicationExtension
 import com.android.build.api.dsl.LibraryExtension
 
 interface ScaffoldPluginExtension {
@@ -7,42 +6,25 @@ interface ScaffoldPluginExtension {
 }
 
 val scaffold = extensions.create<ScaffoldPluginExtension>("scaffold")
-scaffold.launchAsApplication.convention(project.findProperty("scaffold.launchAsApplication")?.toString()?.toBoolean() ?: false)
-scaffold.namespace.convention(project.findProperty("scaffold.namespace")?.toString())
+scaffold.launchAsApplication.convention(
+    project.findProperty("scaffold.launchAsApplication")?.toString()?.toBoolean() ?: false
+)
 
 if (scaffold.launchAsApplication.get()) {
     apply(plugin = "foodiestudio.android.application.compose")
-    the<ApplicationExtension>().apply {
-        namespace = scaffold.namespace.get()
-        defaultConfig { testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner" }
-    }
 } else {
     apply(plugin = "foodiestudio.android.library.compose")
     apply(plugin = "maven-publish")
     the<LibraryExtension>().apply {
-        namespace = scaffold.namespace.get()
         defaultConfig {
-            testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
             consumerProguardFiles("consumer-rules.pro")
-        }
-        buildTypes {
-            release {
-                isMinifyEnabled = false
-                setProguardFiles(
-                        listOf(
-                                getDefaultProguardFile("proguard-android-optimize.txt"),
-                                "proguard-rules.pro"
-                        )
-                )
-            }
         }
         publishing { singleVariant("release") { withSourcesJar() } }
     }
-
     group = project.findProperty("scaffold.publishGroup")?.toString()!!
     version = project.findProperty("scaffold.publishVersion")?.toString()!!
 
-    the<PublishingExtension>().apply {
+    configure<PublishingExtension> {
         publications {
             register<MavenPublication>("release") { afterEvaluate { from(components["release"]) } }
         }
--- devtools/build.gradle.kts
+++ devtools/build.gradle.kts
@@ -2,6 +2,25 @@ plugins {
     id("scaffold")
 }
 
+android {
+    namespace = "com.github.foodiestudio.devtools"
+
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            setProguardFiles(
+                listOf(
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
+                )
+            )
+        }
+    }
+}
+
 dependencies {
     implementation(sharedLibs.bundles.compose.core)
     implementation(sharedLibs.accompanist.systemuicontroller)
